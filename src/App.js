8d7d8e6260af28376005f42510c2a8b1359e981f
SanjanaPS29 <sanjana.softsuave2022@gmail.com>
2022-02-22 17:54:35 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Simple from './Simple';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+    <Simple/>
+    </div>
+  );
+}
+
+export default App;
